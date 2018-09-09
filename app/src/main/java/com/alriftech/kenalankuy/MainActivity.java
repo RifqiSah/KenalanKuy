6edfb3abeddf6ecbac61043b37f4421bd2cce44d
RifqiSah <rifqi_sah@yahoo.com>
2018-09-09 22:34:25 +0700

    Login & Register

--- app/src/main/java/com/alriftech/kenalankuy/MainActivity.java
+++ app/src/main/java/com/alriftech/kenalankuy/MainActivity.java
@@ -1,9 +1,11 @@
 package com.alriftech.kenalankuy;
 
+import android.content.Intent;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+    }
+
+    public void Login(View v) {
+        Intent i = new Intent(getApplicationContext(), Login.class);
+        startActivity(i);
+    }
+
+    public void Register(View v) {
+        Intent i = new Intent(getApplicationContext(), Register.class);
+        startActivity(i);
     }
 }

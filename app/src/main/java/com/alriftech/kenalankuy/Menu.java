0af6e730544aa9064245317eef9b120f8fa98e39
Achmad Dejan F <deanfirdianza@gmail.com>
2018-09-10 03:26:26 +0700

    Commit Dejan

--- app/src/main/java/com/alriftech/kenalankuy/Menu.java
+++ app/src/main/java/com/alriftech/kenalankuy/Menu.java
@@ -26,7 +26,7 @@ public class Menu extends AppCompatActivity {
             public void onClick(View view) {
                 IntentIntegrator integrator = new IntentIntegrator(activity);
                 integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setPrompt("Scan QR Code Teman");
+                integrator.setPrompt("Scan QR Code Teman Anda");
                 integrator.setCameraId(0);
                 integrator.setBeepEnabled(true);
                 integrator.setOrientationLocked(true);

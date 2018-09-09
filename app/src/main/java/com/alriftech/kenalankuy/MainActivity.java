2553f50c2fff004e6731a3f894f9805ad0cf47a1
RifqiSah <rifqi_sah@yahoo.com>
2018-09-09 21:12:28 +0700

    Add onCreate

--- app/src/main/java/com/alriftech/kenalankuy/MainActivity.java
+++ app/src/main/java/com/alriftech/kenalankuy/MainActivity.java
@@ -1,5 +1,7 @@
 package com.alriftech.kenalankuy;
 
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
     }
 }
